f1de8fbb1bfe2a30b9cbe173ec1fc0cd79e3f3b8
Gabriel <106556645+GabrielNascimento2611@users.noreply.github.com>
2025-03-01 02:04:19 +0000

    Organizado Em Pastas De Cada Nivel

---
+++ Nivel Aventureiro/ModularidadeEAbstracao.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Função genérica para entrada de dados
+void esntradaDados() {
+    //código para a dunção entradaDados
+}
+
+// Função genérica para calculo da média
+float calcularMedio() {
+    // código para a função calcularMedia
+}
+
+// Função genérica para exibir dados
+void exibirMedia() {
+    // código para a função exibirMedia
+    }
+
+int main() {
+    entradaDados();
+    float media = calcularMedia();
+    exibirMedia();
+    return 0;
+    
+}
